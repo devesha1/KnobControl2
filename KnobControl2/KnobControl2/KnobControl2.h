ffb8f5fd5c6d5bc527fd5f6ca7d04418502be0ed
Devesh <>
2019-05-06 19:24:14 +0530

    Initial commit

---
+++ KnobControl2/KnobControl2/KnobControl2.h
@@ -0,0 +1,19 @@
+//
+//  KnobControl2.h
+//  KnobControl2
+//
+//  Created by Devesh on 06/05/19.
+//  Copyright © 2019 cisco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KnobControl2.
+FOUNDATION_EXPORT double KnobControl2VersionNumber;
+
+//! Project version string for KnobControl2.
+FOUNDATION_EXPORT const unsigned char KnobControl2VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KnobControl2/PublicHeader.h>
+
+
